db2192e37da36dd767eb8d867a82cbb2e410155f
Thiediev <chanjection@gmail.com>
2014-11-01 06:20:45 -0500

    Revert "gt rid f acid"
    This reverts commit 62c9dc8da6db7672d0a2665d664aa01aa008d2ce.

--- data/moves.js
+++ data/moves.js
@@ -1,5 +1,20 @@
 exports.BattleMovedex = {
-
+	"absorb": {
+		num: 71,
+		accuracy: 100,
+		basePower: 20,
+		category: "Special",
+		desc: "Deals damage to one adjacent target. The user recovers half of the HP lost by the target, rounded up. If Big Root is held by the user, the HP recovered is 1.3x normal, rounded half down.",
+		shortDesc: "User recovers 50% of the damage dealt.",
+		id: "absorb",
+		name: "Absorb",
+		pp: 25,
+		priority: 0,
+		drain: [1, 2],
+		secondary: false,
+		target: "normal",
+		type: "Grass"
+	},
 	"acid": {
 		num: 51,
 		accuracy: 100,
