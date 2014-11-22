1e744ff680edf5d2b53c5b8ee67b611e12f1a08b
Thiediev <chanjection@gmail.com>
2014-11-21 22:47:45 -0600

    Revert "Update scripts.js"
    This reverts commit a87e645b6dcc1052154b3527e4d368bad22692f8.

--- mods/gen1/scripts.js
+++ mods/gen1/scripts.js
@@ -10,10 +10,8 @@ exports.BattleScripts = {
 			this.add('debug', activity);
 		}
 	},
-	{
 	init: function () {this.modData('Learnsets', 'gyarados').learnset.skyattack = ['5L100'];
 	}
-	},
 	// getStat callback for gen 1 stat dealing
 	getStatCallback: function (stat, statName, pokemon) {
 		// Hard coded Reflect and Light Screen boosts
