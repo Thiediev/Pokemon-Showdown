aafe824ed670562e5055af15ba5dcd7c757f8e9d
Thiediev <chanjection@gmail.com>
2015-04-29 08:25:31 -0500

    added moves
    boneclub through explosion

--- mods/rbyplus/moves.js
+++ mods/rbyplus/moves.js
@@ -57,11 +57,149 @@ exports.BattleMovedex = {
 		basePower: 80,
 		secondary: {
 			chance: 30,
-			volatilestatus: 'flinch'
+			volatileStatus: 'flinch'
 		}
 	},
 	blizzard: {
 		inherit: true,
 		accuracy: 80
+	},
+	boneclub: {
+		inherit: true,
+		type: "Ghost",
+		priority: 1,
+		basePower: 60
+	},
+	bonemerang: {
+		inherit: true,
+		type: "Ghost",
+		basePower: 60
+	},
+	bubble: {
+		inherit: true,
+		basePower: 80,
+		secondary: {
+			chance: 30,
+			volatileStatus: 'flinch'
+		}
+	},
+	bubblebeam: {
+		inherit: true,
+		basePower: 150,
+		self: {
+			volatileStatus: 'mustrecharge'
+		}
+	},
+	clamp: {
+		inherit: true,
+		basePower: 15
+	},
+	cometpunch: {
+		inherit: true,
+		type: "Fighting",
+		multihit: false,
+		basePower: 80,
+		priority: 1
+	},
+	confusion: {
+		inherit: true,
+		basePower: 120,
+		accuracy: 85
+	},
+	constrict: {
+		inherit: true,
+		category: "Status",
+		status: 'par'
+	},
+	crabhammer: {
+		inherit: true,
+		basePower: 90,
+		accuracy: 100
+	},
+	cut: {
+		inherit: true,
+		critRatio: 2,
+		type: "Bug"
+		basePower: 55
+	},
+	defensecurl: {
+		inherit: true,
+		heal: [1,2],
+		pp: 5
+	},
+	dig: {
+		inherit: true,
+		basePower: 150,
+		accuracy: 85
+	},
+	dizzypunch: {
+		inherit: true,
+		basePower: 150,
+		accuracy: 75,
+		secondary: {
+			chance: 10,
+			volatileStatus: 'confusion'
+		}
+	},
+	doublekick: {
+		inherit: true,
+		basePower: 50
+	},
+	doubleteam: {
+		inherit: true,
+		secondary: false,
+		volatileStatus: 'confusion'
+	},
+	doubleedge: {
+		inherit: true,
+		basePower: 120
+	},
+	doubleslap: {
+		inherit: true,
+		type: "Psychic",
+		basePower: 60,
+		multihit: 2
+	},
+	dragonrage: {
+		inherit: true,
+		basePower: 120,
+		damage: 0,
+		pp: 5
+	},
+	dreameater: {
+		inherit: true,
+		basePower: 250,
+		type: "Psychic",
+		drain: [1, 2]
+	},
+	drillpeck: {
+		inherit: true,
+		basePower: 90
+	},
+	eggbomb: {
+		inherit: true,
+		basePower: 100,
+		type: "Fire",
+		pp: 5,
+		secondary: {
+			chance: 30,
+			status: 'brn'
+		}
+	},
+	ember: {
+		inherit: true,
+		basePower: 90,
+		secondary: {
+			chance: 30,
+			boosts: {
+				spa: -1,
+				spd: -1
+			}
+		}
+	},
+	explosion: {
+		inherit: true,
+		basePower: 100,
+		type: "Rock"
 	}
 };
